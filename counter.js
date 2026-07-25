84d8ef019bf6809d2f8f8feceaadd0c24d75d676
Stiefel07 <m_luko@gmx.de>
2026-07-25 13:14:26 +0700

    Refactor value update logic and remove autoFill function
    Removed the autoFill function and simplified logic for updating values based on changes.

--- counter.js
+++ counter.js
@@ -15,24 +15,7 @@ function autoFill(changed){
     }
 
     if(changed==="arc"){
-        if(i + a <= 100) cav.value = 100 - i - a;
-    }
-}
-function autoFill(changed){
-    let i = parseInt(inf.value) || 0;
-    let c = parseInt(cav.value) || 0;
-    let a = parseInt(arc.value) || 0;
-
-    if(changed==="inf"){
-        if(i + c <= 100) arc.value = 100 - i - c;
-    }
-
-    if(changed==="cav"){
-        if(i + c <= 100) arc.value = 100 - i - c;
-    }
-
-    if(changed==="arc"){
-        if(i + a <= 100) cav.value = 100 - i - a;
+        if(i + a <= 100) cav.value = 100 - i - 
     }
 }
 function calc(){
