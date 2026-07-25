fa710afd276e1fcfe9e65b7125b81b72068a80ea
Stiefel07 <m_luko@gmx.de>
2026-07-25 09:51:05 +0700

    Implement autoFill function for counter logic
    Add autoFill function to manage input values for counter.

--- counter.js
+++ counter.js
@@ -1,3 +1,20 @@
 // Kingshot Counter Tool
 // Counter Logic
 // © Stiefel™
+function autoFill(changed){
+    let i = parseInt(inf.value) || 0;
+    let c = parseInt(cav.value) || 0;
+    let a = parseInt(arc.value) || 0;
+
+    if(changed==="inf"){
+        if(i + c <= 100) arc.value = 100 - i - c;
+    }
+
+    if(changed==="cav"){
+        if(i + c <= 100) arc.value = 100 - i - c;
+    }
+
+    if(changed==="arc"){
+        if(i + a <= 100) cav.value = 100 - i - a;
+    }
+}
