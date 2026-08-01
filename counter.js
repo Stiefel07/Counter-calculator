b29943a357d923a5c79d96d7f63405f3ed067d9c
Stiefel07 <m_luko@gmx.de>
2026-08-01 16:41:24 +0700

    Calculate gap between highest and second highest values

--- counter.js
+++ counter.js
@@ -243,6 +243,8 @@ const sorted = Object.entries(vals).sort((a,b)=>b[1]-a[1]);
 const enemy = sorted[0][0];
 const highest = sorted[0][1];
 const second = sorted[1][1];
+const gap = highest - second;
+const second = sorted[1][1];
 const tie = highest === second;
 const counter = {
     Infantry:"Marksmen",
